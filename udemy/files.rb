5d3593d2de5e5cbba174ecf66129d6229988c9cd
dzmitrytrystsen <tristendmitry@gmail.com>
2019-05-10 17:09:05 +0300

    require

---
+++ udemy/files.rb
@@ -0,0 +1,19 @@
+# File.open('text.txt').each do |line|
+#   p line
+# end
+#
+# File.open('text.txt', 'a') do |file|
+#   file.puts 'Creating first line with Ruby! (with puts)'
+#   file.write 'Creating second line with Ruby! (with write)'
+#   end
+#
+# File.open('text1.txt', 'w') do |file|
+#   file.puts 'Creating first line with Ruby! (with puts)'
+#   file.write 'Creating second line with Ruby! (with write)'
+# end
+#
+# File.open('text1.txt').each do |line|
+#   p line
+# end
+#
+# File.rename('text1.txt', 'text_renamed.txt')
---
+++ udemy/load/start.rb
@@ -0,0 +1,11 @@
+p 'The beginning file'
+
+# load 'end.rb'
+#
+# p 'end.rb was loaded'
+
+require './end.rb' # works only once
+require './end.rb'
+
+require_relative 'end' # looks in current directory
+
