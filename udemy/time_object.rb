0fac09ef674ccd82713fd2c9d497d5871e8af877
dzmitrytrystsen <tristendmitry@gmail.com>
2019-05-10 14:58:46 +0300

    Files

--- udemy/time_object.rb
+++ udemy/time_object.rb
@@ -1,7 +1,27 @@
+# birthday = Time.new(1994, 12, 22)
+# today = Time.now
+#
+# p today.hour
+# p today.yday # day of a current year
+#
+# p birthday.dst?
+
+# def find_day_of_year_by_number(number)
+#   current_day = Time.new(2016, 1, 1)
+#   one_day = 60 * 60 * 24
+#
+#   until current_day.yday == number
+#     current_day += one_day
+#   end
+#
+#   current_day
+# end
+#
+# p find_day_of_year_by_number(4)
+
 birthday = Time.new(1994, 12, 22)
-today = Time.now
+p birthday.ctime #convert to string hrf
+p birthday.strftime('%B %d, %Y') # custom string representaion + there are a lot of other arg
 
-p today.hour
-p today.yday # day of a current year
-
-p birthday.dst?
+require 'time'
+p Time.parse('2016-01-01')
