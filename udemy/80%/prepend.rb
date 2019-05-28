dec8cc27e47db189f58c085765b509cef8553d66
dzmitrytrystsen <tristendmitry@gmail.com>
2019-05-28 13:01:09 +0300

    prepend

--- udemy/80%/prepend.rb
+++ udemy/80%/prepend.rb
@@ -8,6 +8,11 @@ class BookStore
   include Purchaseable
 end
 
+class BookStore1
+  prepend Purchaseable
+end
+
 barnes_and_noble = BookStore.new
 p barnes_and_noble.purchase('Atlas')
 p BookStore.ancestors
+p BookStore1.ancestors
